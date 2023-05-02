1ad04bbd49834e8c0290a0ffa4a92349da136d56
shamim26 <shamimreja101@gmail.com>
2023-05-02 17:16:49 +0600

    chef card added

--- src/pages/Home/Chef's/Chef.jsx
+++ src/pages/Home/Chef's/Chef.jsx
@@ -5,7 +5,7 @@ const Chef = ({ chef }) => {
   const { name, chefId, likes, numberOfRecipes, yearsOfExperience, picture } =
     chef;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-md rounded-none">
       <figure>
         <img
         className="h-[288px]"
@@ -14,10 +14,10 @@ const Chef = ({ chef }) => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title">{name}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary rounded-none">Buy Now</button>
         </div>
       </div>
     </div>
